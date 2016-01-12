4db267b51f50525b32d08499764e5d64027ae121
Alexander Lobanov <alex_l@inbox.ru>
2016-01-12 12:05:37 +0300

    first commit

---
+++ CardScreen/CardScreen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardScreen
+//
+//  Created by Alexander Lobanov on 12.01.16.
+//  Copyright © 2016 Omega-R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
